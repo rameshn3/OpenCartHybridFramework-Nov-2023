3bb849940b6209e3046fb0ac25e2cbfe6228c327
Ramesh Ch <rameshn3@gmail.com>
2023-11-21 18:20:52 -0700

    Create SearchPageTest.java
    Create SearchPageTest.java

---
+++ src/test/java/com/qa/opencart/testcase/SearchPageTest.java
@@ -0,0 +1,22 @@
+package com.qa.opencart.testcase;
+import org.testng.annotations.Test;
+
+import com.qa.opencart.factory.TestBase;
+import com.qa.opencart.pages.HomePage;
+import com.qa.opencart.pages.LoginPage;
+import com.qa.opencart.pages.RegistrationPage;
+import com.qa.opencart.utils.Constants;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+public class SearchPageTest extends TestBase{
+private static Logger log=LogManager.getLogger(SearchPageTest.class.getName());
+ 
+
+
+}
